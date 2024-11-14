a95baae73edc299ff9c3b45aad9759decde5b787
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2024-11-14 19:14:55 +0000

    Create grid in sketch.js

--- sketch.js
+++ sketch.js
@@ -4,4 +4,13 @@ function setup() {
 
 function draw() {
   background(220);
+
+  for (var x = 0; x < width; x += width / 10) {
+    for (var y = 0; y < height; y += height / 9) {
+      stroke(0);
+      strokeWeight(1);
+      line(x, 0, x, height);
+      line(0, y, width, y);
+    }
+  }
 }
